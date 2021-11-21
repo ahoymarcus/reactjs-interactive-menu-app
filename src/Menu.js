1880498b84f384adad948efac9ac84ceef4d3ac3
Marcus Vinícius da Silveira richa <marcus.richa@sempreceub.com>
2021-11-21 20:41:38 -0300

    Definir estrutura basica Jsx do componente Menu, renderizando as informacoes recebidas como props. Mudar Media-query de estilo

--- src/Menu.js
+++ src/Menu.js
@@ -1,7 +1,31 @@
 import React from 'react';
 
-const Menu = () => {
-  return <h2>menu component</h2>;
+const Menu = ({ items }) => {
+
+  const renderItem = items.map((item) => {
+    const { id, title, category, img, price, desc } = item;
+
+    return <article key={id} className="menu-item">
+      <img src={img} alt={title} className="photo" />
+      <div className="item-info">
+        <header>
+          <h4>{title}</h4>
+          <h4 className="price" >${price}</h4>
+        </header>
+        <p className="item-text">{desc}</p>
+      </div>
+    </article> 
+  });
+
+  return (
+    <div className="section-center">
+      {renderItem}
+    </div>
+  );
 };
 
+
+
 export default Menu;
+
+
